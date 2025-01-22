b3dd08a18613abf4e6754547fd844779082d362e
azi-prac <azimemory@gmail.com>
2025-01-22 11:12:38 +0900

    lec: 변수선언

--- src/e_coffeemanager/Run.java
+++ src/e_coffeemanager/Run.java
@@ -4,8 +4,31 @@ import java.util.Scanner;
 
 public class Run {
     public static void main(String[] args) {
+        int balance = 100000;
+        int expenses = 0;
+        int salesPrice = 0;
 
+        String americanoName = "americano";
+        int americanoPrice = 1000;
+        int americanoPurchasePrice = 500;
+        int americanoStock = 10;
+        int americanoSafetyStock = 3;
+        int americanoSalesCnt = 0;
 
+        String mochaName = "mocha";
+        int mochaPrice = 2000;
+        int mochaPurchasePrice = 800;
+        int mochaStock = 10;
+        int mochaSafetyStock = 3;
+        int mochaSalesCnt = 0;
 
+        String latteName = "latte";
+        int lattePrice = 3000;
+        int lattePurchasePrice = 100000;
+        int latteStock = 10;
+        int latteSafetyStock = 3;
+        int latteSalesCnt = 0;
+
+        Scanner sc = new Scanner(System.in);
     }
 }
