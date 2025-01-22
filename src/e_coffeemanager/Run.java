e326f7a6d8e1711fbc97c13137dc1cca8d255a48
azi-prac <azimemory@gmail.com>
2025-01-22 11:13:30 +0900

    lec: menu

--- src/e_coffeemanager/Run.java
+++ src/e_coffeemanager/Run.java
@@ -30,5 +30,29 @@ public class Run {
         int latteSalesCnt = 0;
 
         Scanner sc = new Scanner(System.in);
+
+        while(true) {
+
+            System.out.println("\n=========Menu=========");
+            System.out.println("판매등록(1)");
+            System.out.println("현황(2)");
+            System.out.println("종료(3)");
+            System.out.print("입력 : ");
+
+            int menu = sc.nextInt();
+
+            if(menu < 1 || menu > 3) {
+                System.out.println("* 올바른 매뉴번호를 입력해 주세요");
+                continue;
+            }
+
+            if(menu == 1) {
+
+            }else if(menu == 2) {
+
+            }else  {
+
+            }
+        }
     }
 }
